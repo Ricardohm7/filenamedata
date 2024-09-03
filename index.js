70ea1c89658569fc360beb0879f8665e2c4cfd67
Ricardo Hernández <ricardo.her.mendez@gmail.com>
2024-09-03 12:17:48 -0600

    filename data completed

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require("multer")
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+const upload = multer({ dest: './public/data/uploads' })
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+// method="POST" action="/api/fileanalyse"
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  const { originalname, size, mimetype } = req.file
+  res.json({
+    name: originalname,
+    type: mimetype,
+    size
+  })
+})
 
 
 
